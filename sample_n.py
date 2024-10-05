9864d807b6ba389c08bcef981170faffc2f7c463
L Lllvvuu <git@llllvvuu.dev>
2024-10-04 23:23:16 -0700

    fix: inference

--- sample_n.py
+++ sample_n.py
@@ -2,7 +2,7 @@ import sys
 from vllm import LLM, SamplingParams
 
 n = int(sys.argv[1]) if len(sys.argv) > 1 else 10
-model = sys.argv[2] if len(sys.argv) > 2 else "Qwen/Qwen2.5-32B"
+model = sys.argv[2] if len(sys.argv) > 2 else "Qwen/Qwen2.5-Coder-7B"
 prompt = sys.stdin.read()
 prompts = [prompt for _ in range(n)]
 sampling_params = SamplingParams()
@@ -11,4 +11,4 @@ outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    print(f"Prompt: {prompt}, Generated text: {generated_text}")
