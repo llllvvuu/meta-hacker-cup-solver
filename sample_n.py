586a7ccdc47e7659b70adb966a4f17d287714c57
L Lllvvuu <git@llllvvuu.dev>
2024-10-05 00:46:45 -0700

    fix: stopping

--- sample_n.py
+++ sample_n.py
@@ -4,7 +4,7 @@ from vllm import LLM, SamplingParams
 n = int(sys.argv[1]) if len(sys.argv) > 1 else 32
 model = sys.argv[2] if len(sys.argv) > 2 else "Qwen/Qwen2.5-14B"
 prompts = [sys.stdin.read()]
-sampling_params = SamplingParams(max_tokens=1024, n=n)
+sampling_params = SamplingParams(max_tokens=2048, n=n, stop="</problem>")
 llm = LLM(model=model, max_model_len=8192, swap_space=32)
 outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
