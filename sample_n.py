9c43cb0b370e443774e76bf2fbe0b6de154c5384
L Lllvvuu <git@llllvvuu.dev>
2024-10-05 00:32:59 -0700

    fix: swap space

--- sample_n.py
+++ sample_n.py
@@ -5,7 +5,7 @@ n = int(sys.argv[1]) if len(sys.argv) > 1 else 32
 model = sys.argv[2] if len(sys.argv) > 2 else "Qwen/Qwen2.5-14B"
 prompts = [sys.stdin.read()]
 sampling_params = SamplingParams(max_tokens=1024, n=n)
-llm = LLM(model=model, max_model_len=8192)
+llm = LLM(model=model, max_model_len=8192, swap_space=32)
 outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
     prompt = output.prompt
